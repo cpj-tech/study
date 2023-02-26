d0f13b933bf709ade4a272c0162c2cebd18efa71
yoshikikasama <kasamayoshiki@gmail.com>
2023-02-26 18:43:01 +0900

    20230226

--- python/algorithm/calendar/kasama/calendar_create.py
+++ python/algorithm/calendar/kasama/calendar_create.py
@@ -16,12 +16,17 @@ def main():
     prev_month_days = calendar.monthrange(year, month - 1)[1] if month != 1 else calendar.monthrange(year - 1, 12)[1]
 
     # 月の日数を取得
+    # calendar.monthrange: return 月の最初の曜日, 月の日数
     days = calendar.monthrange(year, month)[1]
     # 最終日をdate型に変換
     last_day = datetime.date(year, month, days)
 
     # カレンダーを作成
     cal = calendar.monthcalendar(year, month)
+    print("cal:", cal)
+    # cal: [[0, 0, 0, 1, 2, 3, 4], [5, 6, 7, 8, 9, 10, 11],
+    # [12, 13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24, 25],
+    #  [26, 27, 28, 0, 0, 0, 0]]
     # 前月の日付の埋め方
     # 0:月=1埋める,
     # 1:火=2埋める,
@@ -35,7 +40,6 @@ def main():
     for _ in range(roop_range):
         cal[0].insert(0, prev_month_days)
         prev_month_days -= 1
-    cals = []
     # 次月の日付けの埋め方
     # 0:月=5埋める,
     # 1:火=4埋める,
@@ -49,21 +53,17 @@ def main():
     for i in range(roop_range):
         print(i + 1)
         cal[-1].append(i + 1)
-    cals = []
+    # カレンダーを出力
+    print("cal:", cal)
+    print("days(末日):", days)
+    print("日  月  火  水  木  金  土")
+    # cal: [[26, 27, 28, 1, 2, 3, 4], [5, 6, 7, 8, 9, 10, 11],
+    # [12, 13, 14, 15, 16, 17, 18], [19, 20, 21, 22, 23, 24, 25],
+    #  [26, 27, 28, 29, 30, 31, 1]]
     for week in cal:
         week = [i for i in week if i != 0]
-        cals.append(week)
-    # カレンダーを出力
-    print("日  月  火  水  木  金  土")
-    for week in cals:
-        week = [i for i in week if i != 0]
         for day in week:
-            if day > days:
-                print("{:2d} ".format(day), end=" ")
-            elif day <= (31 if days >= 31 else 30):
-                print("{:2d} ".format(day), end=" ")
-            else:
-                print(" {:2d}".format(day), end=" ")
+            print("{:2d} ".format(day), end=" ")
         print("")
 
 
