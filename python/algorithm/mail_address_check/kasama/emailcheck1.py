b7d5b2433e6be455784a238435acf01a2d179014
yoshikikasama <kasamayoshiki@gmail.com>
2024-02-26 22:31:06 +0900

    practice

--- python/algorithm/mail_address_check/kasama/emailcheck1.py
+++ python/algorithm/mail_address_check/kasama/emailcheck1.py
@@ -1,22 +1,60 @@
-
 def is_alphanumeric_mixed_case(letter):
-    return "0" <= letter <= "9" or "A" <= letter <= "Z" or \
-    "a" <= letter <= "z"
+    return "0" <= letter <= "9" or "A" <= letter <= "Z" or "a" <= letter <= "z"
+
 
 def main():
     input_address = input("文字列-->")
     print(input_address)
     state = "HEAD"
+    count = 0
     for letter in input_address:
+        count += 1
         if state == "HEAD":
-                if letter == ".":
-                    print("NG")
-                elif state == "HEAD" and is_alphanumeric_mixed_case(letter):
-                    state = "LOCAL"
-                    continue
+            if letter == "." or not is_alphanumeric_mixed_case(letter):
+                state = "NG"
+                break
+            elif is_alphanumeric_mixed_case(letter):
+                state = "LOCAL"
+                continue
         elif state == "LOCAL":
-                if letter == ".":
-                    state == "LOCAL_DOT"
+            if letter == ".":
+                state = "LOCAL_DOT"
+                continue
+            elif is_alphanumeric_mixed_case(letter):
+                continue
+            elif letter == "@":
+                state = "HOST"
+            else:
+                state = "NG"
+                break
+        elif state == "LOCAL_DOT":
+            if letter == "." or not is_alphanumeric_mixed_case(letter):
+                state = "NG"
+                break
+            else:
+                state = "LOCAL"
+                continue
+        elif state == "HOST":
+            if letter == ".":
+                if count == len(input_address):
+                    state = "NG"
+                    break
+                state = "HOST_DOT"
+                continue
+            elif is_alphanumeric_mixed_case(letter):
+                continue
+        elif state == "HOST_DOT":
+            if letter == "." or not is_alphanumeric_mixed_case(letter):
+                state = "NG"
+                break
+            else:
+                state = "HOST"
+                continue
+
+    if state == "HOST":
+        print("メールアドレスです。")
+    else:
+        print("メールアドレス形式に誤りがあります。")
 
 
 if __name__ == "__main__":
